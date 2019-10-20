217e77dc793280d2e6cab780f0f4e78962051bfd
Prasanna <prasannaparthasarathi@Prasannas-MacBook-Pro.local>
2019-10-20 14:14:09 -0400

    added dynamic mask

--- Utils/Bert_util.py
+++ Utils/Bert_util.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import os
 
 #/# Load Embeddings
-def load_embeddings(args):
-    if args.dataset == 'mwoz':
+def Load_embeddings(dataset):
+    if dataset == 'mwoz':
         embeddings_file = "mwoz_embedding.txt"
     else:
         embeddings_file = "frames_embedding.txt"
@@ -19,9 +19,20 @@ def load_embeddings(args):
             embs.append(coefs)
     return words, torch.tensor(embs)
 
-def bert_metric(output_embeddings, target_embeddings):
+def Bert_loss(output_embeddings, target_embeddings):
     output_sen_embedding = torch.mean(output_embeddings, dim=1)
     target_sen_embedding = torch.mean(target_embeddings, dim=1)
     mse_loss = F.mse_loss(output_sen_embedding, target_sen_embedding, reduction='none')
     mse_loss = torch.mean(mse_loss,dim = 1)
     return mse_loss
+
+def Mask_sentence(res, tar, mask_, mask_ind=3):
+    res_masked = res.flatten()
+    tar_masked = tar.flatten()
+    mask = ~torch.Tensor(mask_).bool()
+    mask = torch.index_select(mask, dim=0, index=tar_masked)
+    tar_masked.masked_fill_(mask, mask_ind)
+    res_masked.masked_fill_(mask, mask_ind)
+    tar_masked = tar_masked.reshape_as(tar)
+    res_masked = res_masked.reshape_as(res)
+    return res_masked, tar_masked
