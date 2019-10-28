11617e5f2e2a8d5fd26f4966d5f98ec87568176d
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-10-28 14:33:07 -0400

    Add prebert mask option

--- Utils/Bert_util.py
+++ Utils/Bert_util.py
@@ -28,24 +28,25 @@ def Bert_loss(output_embeddings, target_embeddings):
     return mse_loss
 
 
-def Mask_sentence(res, mask_, config):
+def Mask_sentence(res, mask, config):
+    # mask_ind here corresponds to the index of the <pad> words
     mask_ind = config['pad_index']
     eos_ind = config['eos_index']
-    no_posteos_mask = config['no_posteos_mask']
+    posteos_mask = config['posteos_mask']
     device = config['device']
     res_masked = res.flatten()
-    mask = torch.from_numpy(mask_).to(device).bool()
+    # mask = torch.from_numpy(mask_).to(device).bool()
     mask = torch.index_select(~mask, dim=0, index=res_masked)
     res_masked = res_masked.masked_fill(mask, mask_ind)
     res_masked = res_masked.reshape_as(res)
     # Apply a mask on the words after the first <eos> generated
-    if not no_posteos_mask:
-        posteos_mask = res == eos_ind
+    if posteos_mask:
+        mask_posteos = res == eos_ind
         indices = np.arange(res.shape[1])
         indices = torch.from_numpy(indices).to(device)
         indices = indices.repeat(res.shape[0], 1)
-        eos_positions = indices.masked_fill(~posteos_mask, 100000)
+        eos_positions = indices.masked_fill(~mask_posteos, 100000)
         eos_positions = torch.argmin(eos_positions, dim=1).unsqueeze(1).expand_as(indices)
-        posteos_mask = indices <= eos_positions
-        res_masked = res_masked.masked_fill(~posteos_mask, mask_ind)
+        mask_posteos = indices <= eos_positions
+        res_masked = res_masked.masked_fill(~mask_posteos, mask_ind)
     return res_masked
